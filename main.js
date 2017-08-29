006e7ceddfa800272e23f0b4be20a976f7fd30f2
anna-cantrell <dcxdesigns@gmail.com>
2017-08-29 11:04:44 -0400

    small changes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 907, height: 575})
+  win = new BrowserWindow({width: 907, height: 595})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -19,7 +19,7 @@ function createWindow () {
 
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
